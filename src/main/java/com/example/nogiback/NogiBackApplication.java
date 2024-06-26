d1215f20f7abc371812d9617266008e713e24ac5
Eric Wang <kaedewang0726@gmail.com>
2024-06-26 17:11:10 +1000

    the first commit

---
+++ src/main/java/com/example/nogiback/NogiBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.nogiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NogiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nogiback/NogiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nogiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NogiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
